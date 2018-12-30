f8faa1f1b43e511ac23b5f6f87a204eda214f303
ALEX-HJY <156979387@qq.com>
2018-12-30 15:29:47 +0800

    init

--- a3_back_test.py
+++ a3_back_test.py
@@ -11,31 +11,30 @@ dff = get_data()
 START_MONEY = 1000000
 
 
-def back_test(d=pd.DataFrame(), y_name='', start_year='2017', end_year='2018',ma1=5,ma2=30,ma3=5,ma4=20):
+def back_test(d=pd.DataFrame(), y_name='', start_year='2017', end_year='2018',ma1=5,ma2=10,ma3=5,ma4=15):
     money = 1000000
     share = 0
     df = deepcopy(d)
     df = df[start_year:end_year]
     df = df[[y_name, 'CLOSE']]
-    df = df.dropna(inplace=True)
+
     df['bench'] = START_MONEY
     share = START_MONEY / df.iloc[29].loc['CLOSE']
     df[y_name + '_share'] = 0
     df[y_name + '_money'] = START_MONEY
     df[y_name + '_hold'] = False
-    df = df.dropna(inplace=True)
     for i in range(30, len(df)):
         # print(i)
         df.ix[i,'bench'] = share * df.ix[i, 'CLOSE']
-        if df[y_name][i - ma1 - 1:i - 1].mean() < df[y_name][i - ma2 - 1:i - 1].mean() and df[y_name][ i - ma1:i].mean()\
-                > df[ y_name][ i - ma2:i].mean() and not \
+        if df[y_name][i - ma1 - 1:i - 1].mean() > df[y_name][i - ma2 - 1:i - 1].mean() and df[y_name][ i - ma1:i].mean()\
+                < df[ y_name][ i - ma2:i].mean() and not \
                 df[y_name + '_hold'].iloc[i - 1]:
             df.ix[i, y_name + '_hold'] = True
             df.ix[i, y_name + '_share'] = df.ix[i - 1, y_name + '_money'] / df.ix[i, 'CLOSE']
             df.ix[i, y_name + '_money'] = df.ix[i - 1, y_name + '_money']
             continue
-        if df[y_name][i - ma3 - 1:i - 1].mean() > df[y_name][i - ma4 - 1:i - 1].mean() and df[y_name][i - ma3:i].mean()\
-                < df[y_name][i - ma4:i].mean() and \
+        if df[y_name][i - ma3 - 1:i - 1].mean() < df[y_name][i - ma4 - 1:i - 1].mean() and df[y_name][i - ma3:i].mean()\
+                > df[y_name][i - ma4:i].mean() and \
                 df[y_name + '_hold'].iloc[i - 1]:
             df.ix[i, y_name + '_hold'] = False
             df.ix[i, y_name + '_money'] = df.ix[i - 1, y_name + '_share'] * df.ix[i, 'CLOSE']
@@ -51,7 +50,7 @@ def back_test(d=pd.DataFrame(), y_name='', start_year='2017', end_year='2018',ma
             df.ix[i, y_name + '_share'] = df.ix[i - 1, y_name + '_share']
 
     dff = df[[y_name + '_share',y_name + '_money',y_name + '_hold']]
-    print(dff.head())
+    # print(dff.head())
 
     plt.plot(df.index, df[y_name + '_money'], label=y_name)
     plt.plot(df.index, df['bench'], label='HS300', c='r')
@@ -63,9 +62,8 @@ def back_test(d=pd.DataFrame(), y_name='', start_year='2017', end_year='2018',ma
 
 
 if __name__ == '__main__':
-    back_test(dff, '000188_Close')
     back_test(dff, 'VOLUME')
     back_test(dff, 'total_balance')
     back_test(dff, 'PE')
-    back_test(dff, 'PB')
+
     dff.to_csv('test.csv', encoding='utf-8-sig')
